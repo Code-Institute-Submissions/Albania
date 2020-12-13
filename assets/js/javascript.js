f5a2559b190a42ec7266bd90d1280bf895677d11
xhulja14 <xhulja14@gmail.com>
2020-12-13 14:20:17 +0000

    deleted animation functions

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -17,27 +17,7 @@ document.getElementById('form')
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
     });
-});                                      /*----animation----*/
-function rotateAnimation(el,speed){
-	var elem = document.getElementById(el);
-	if(navigator.userAgent.match("Chrome")){
-		elem.style.WebkitTransform = "rotate("+degrees+"deg)";
-	} else if(navigator.userAgent.match("Firefox")){
-		elem.style.MozTransform = "rotate("+degrees+"deg)";
-	} else if(navigator.userAgent.match("MSIE")){
-		elem.style.msTransform = "rotate("+degrees+"deg)";
-	} else if(navigator.userAgent.match("Opera")){
-		elem.style.OTransform = "rotate("+degrees+"deg)";
-	} else {
-		elem.style.transform = "rotate("+degrees+"deg)";
-	}
-	looper = setTimeout('rotateAnimation(\''+el+'\','+speed+')',speed);
-	degrees++;
-	if(degrees > 359){
-		degrees = 1;
-	}
-	document.getElementById("status").innerHTML = "rotate("+degrees+"deg)";
-}                                                  /*---------search-menu---------*/
+});                                              /*---------search-menu---------*/
 function myFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("mySearch");
